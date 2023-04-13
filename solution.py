c588608b926a86588a241cc38ab5809f1fc726be
Владислав Овчинников <81553553+Baradozor@users.noreply.github.com>
2023-04-13 14:58:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,4 +11,5 @@ def solution(x_success: int,
     alpha = 0.06
     _, p_value = proportions_ztest(
         [x_success, y_success], [x_cnt, y_cnt], alternative="smaller"
+    )
     return p_value <= alpha # Ваш ответ, True или False
